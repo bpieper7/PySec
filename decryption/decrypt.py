76809eb976bc66980ad7812207fd163b3b62c36f
nwalsh1995 <nwalsh1995@gmail.com>
2014-02-15 14:13:06 -0600

    will now decrypt all specified types

---
+++ decryption/decrypt.py
@@ -0,0 +1,62 @@
+'''
+The crypt library is not available on windows. Will use the hashlib module.
+
+This file will get hashed password list from PySec\lib\hashedPasswords.txt and then
+    attempt to do a dictionary attack on the basis of user input.
+
+If the user does not input an decryption type then this script will attempt to run
+    all decryption methods.
+'''
+import argparse
+import hashlib
+
+def main():
+    dict = { 'md5' : useMD5, 'sha1' : useSHA1, 'sha224' : useSHA224, 'sha256' : useSHA256, 'sha384' : useSHA384, 'useSHA512' : useSHA512, '' : useAllDecryption }
+    try:
+        parser = argparse.ArgumentParser( description='Enter type of encryption method, none will attempt to run all' )
+        parser.add_argument( 'type', type=str, nargs='*', help='Will only run the md5 decryption method' )
+        args = parser.parse_args()
+        types = args.type
+    except:
+        print 'error getting input'
+        
+    with open( '..\lib\dictionary.txt', 'r' ) as dictionary:
+        with open( '..\lib\hashedPasswords.txt', 'r' ) as hashedPasswords:
+            hashedWords = hashedPasswords.readlines()
+            if not types:
+                useAllDecryption( hashedWords, dictionary )
+            else:
+                for password in dictionary.readlines():
+                    for type in types:
+                        hashedPassword = dict[ type ]( password.strip('\n') )
+                        if hashedPassword in hashedWords:
+                            print "[+] Password is %s for hash %s\n\n" % ( password, hashedPassword )
+                
+def useAllDecryption( hashedWords, dictionary ):
+    for password in dictionary.readlines():
+        if useMD5( password.strip('\n') ) in hashedWords:
+            print "[+] Password is %s for hash %s\n\n" % ( password, useMD5( password ) )
+        elif useSHA1( password.strip('\n') ) in hashedWords:
+            print "[+] Password is %s for hash %s\n\n" % ( password, useSHA1( password ) )
+        elif useSHA224( password.strip('\n') ) in hashedWords:
+            print "[+] Password is %s for hash %s\n\n" % ( password, useSHA224( password ) )
+        elif useSHA256( password.strip('\n') ) in hashedWords:
+            print "[+] Password is %s for hash %s\n\n" % ( password, useSHA256( password ) )
+        elif useSHA512( password.strip('\n') ) in hashedWords:
+            print "[+] Password is %s for hash %s\n\n" % ( password, useSHA512( password ) )
+            
+def useMD5( password ):
+    return hashlib.md5( password ).hexdigest()
+def useSHA1( password ):  
+    return hashlib.sha1( password ).hexdigest()
+def useSHA224( password ):
+    return hashlib.sha224( password ).hexdigest()
+def useSHA256( password ):
+    return hashlib.sha256( password ).hexdigest()
+def useSHA384( password ):
+    return hashlib.sha384( password ).hexdigest()
+def useSHA512( password ):
+    return hashlib.sha512( password ).hexdigest()
+    
+if __name__ == '__main__':
+    main()
