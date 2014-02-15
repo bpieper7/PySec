aa0804baf8c2c463b2dc8fddaa32da47f0286c50
nwalsh1995 <nwalsh1995@gmail.com>
2014-02-15 13:13:08 -0600

    add code to decrypt sha512

--- decryption/decrypt_SHA512.py
+++ decryption/decrypt_SHA512.py
@@ -1,1 +1,16 @@
-placeholder
+'''
+The crypt library is not available on windows. Will use the hashlib module
+'''
+import hashlib
+
+def main():
+    with open( '..\lib\dictionary.txt', 'r' ) as dictionary:
+        with open( '..\lib\hashedPasswords.txt', 'r' ) as hashedPasswords:
+            hashedWords = hashedPasswords.readlines()
+            for password in dictionary.readlines():
+                hashedPassword = hashlib.sha512( password.strip('\n') ).hexdigest()
+                if hashedPassword in hashedWords:
+                    print "[+] Password is %s for hash %s\n\n" % ( password, hashedPassword )
+                
+if __name__ == '__main__':
+    main()
